98aca52dfa175a95cbdd5cc62bf7696657a79642
Nico Tonozzi <nico@nicot.us>
2016-02-21 08:59:13 -0700

    WIP rewrite

--- expose.go
+++ expose.go
@@ -1,120 +1,159 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-	"io"
 	"net"
 	"os"
+	"strconv"
+
+	"github.com/hashicorp/yamux"
+	"io/ioutil"
 )
 
-type options struct {
-	server bool
-	laddr  string
-}
-
-var usage = `Enter a port to expose (like 8080) or server to run expose in server mode.`
-var server = `localhost:4567`
-
-func flags(args []string) options {
-	opts := options{}
-	if len(args) != 2 {
-		badArgs()
-	}
-	if args[1] == "serve" {
-		opts.server = true
-		return opts
-	}
-	/*
-		we should verify arg1 is a valid listening address.
-			n, err := strconv.ParseInt(args[1], 10, 64)
-			if err != nil {
-			badArgs()
-		}*/
-	opts.laddr = args[1]
-	return opts
-}
-
-func badArgs() {
-	fmt.Println(usage)
-	os.Exit(1)
-}
-
 func main() {
-	opts := flags(os.Args)
-	if opts.server {
-		serve()
-		return
-	}
-	connect(opts.laddr)
-}
-
-func log(s ...interface{}) {
-	fmt.Println(s)
-}
-
-// serve listens on a port and proxies connections from the public
-// internet to the program that `connects` back to it.
-func serve() {
-	log("serving")
-	ln, err := net.Listen("tcp", server)
+	err := parseArgs(os.Args)
 	if err != nil {
-		panic(err)
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(-1)
+	}
+}
+
+var errNotEnoughArgs = errors.New("Please provide a port or 'serve' argument.")
+
+type host struct {
+	port     string
+	hostName string
+}
+
+func (h *host) String() string {
+	return h.hostName + h.port
+}
+
+var defaultUpstream = host{
+	port:     ":3800",
+	hostName: "localhost",
+}
+
+func parseArgs(args []string) error {
+	if len(os.Args) < 2 {
+		return errNotEnoughArgs
+	}
+	if os.Args[1] == "serve" {
+		return serve(defaultUpstream.port)
+	}
+	_, err := strconv.ParseUint(os.Args[1], 10, 16)
+	if err != nil {
+		return err
+	}
+	h := host{port: ":" + os.Args[1]}
+	return expose(defaultUpstream, h)
+}
+
+func serve(port string) error {
+	l, err := net.Listen("tcp", port)
+	if err != nil {
+		return err
 	}
 
 	for {
-		conn, err := ln.Accept()
+		conn, err := l.Accept()
 		if err != nil {
-			continue
+			return err
 		}
-		go newService(conn)
+		go handle(upstreamServe(conn))
 	}
+
+	return nil
 }
 
-// A new registered connection, that should have information proxied to it.
-func newService(conn net.Conn) {
-	ln, err := net.Listen("tcp", ":0") // Open up a new high port
+func expose(upstream host, downstream host) error {
+	conn, err := net.Dial("tcp", upstream.String())
 	if err != nil {
-		panic(err)
+		return err
 	}
+	return downstreamServe(conn, downstream)
 }
 
-// connect dials to a public facing server, then proxies packets from
-// the upstream server to the local application at `laddr`.
-func connect(laddr string) {
-	log(laddr)
-	serverConn, err := net.Dial("tcp", server)
+func downstreamServe(conn net.Conn, downstream host) error {
+	session, err := yamux.Server(conn, nil)
 	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	serviceConn, err := net.Dial("tcp", laddr)
-	if err != nil {
-		fmt.Println(err)
-		return
+		return err
 	}
 
-	go copy(serviceConn, serverConn)
-	go copy(serverConn, serviceConn)
-}
+	control, err := session.Accept()
+	if err != nil {
+		return err
+	}
 
-func copy(r io.Reader, w io.Writer) {
-	buf := make([]byte, 2<<16)
+	laddr, err := ioutil.ReadAll(control)
+	fmt.Println(string(laddr))
+	if err != nil {
+		return err
+	}
+
 	for {
-		n, err := r.Read(buf)
+		incoming, err := session.Accept()
 		if err != nil {
-			if err == io.EOF {
-				return
-			}
-			panic(err)
+			return err
 		}
-		buf = buf[:n]
-		var read int
-		for read < n {
-			i, err := w.Write(buf)
-			if err != nil {
-				panic(err)
-			}
-			read += i
+
+		outgoing, err := net.Dial("tcp", downstream.String())
+		if err != nil {
+			return err
 		}
+
+		go handle(proxy(incoming, outgoing))
 	}
+
+	return nil
+}
+
+func handle(err error) {
+	fmt.Fprintln(os.Stderr, err)
+}
+
+func upstreamServe(conn net.Conn) error {
+	l, err := net.Listen("tcp", ":0")
+	if err != nil {
+		return err
+	}
+	session, err := yamux.Client(conn, nil)
+	if err != nil {
+		return err
+	}
+
+	control, err := session.Open()
+	if err != nil {
+		return err
+	}
+
+	laddr := l.Addr().String()
+	if _, err := control.Write([]byte(laddr)); err != nil {
+		return err
+	}
+
+	if err := control.Close(); err != nil {
+		return err
+	}
+
+	for {
+		incoming, err := l.Accept()
+		if err != nil {
+			return err
+		}
+
+		downstream, err := session.Open()
+		if err != nil {
+			return err
+		}
+		go handle(proxy(incoming, downstream))
+	}
+
+	return nil
+}
+
+func proxy(incoming, downstream net.Conn) error {
+	// copy from incoming to downstream and vice versa
+	return nil
 }
